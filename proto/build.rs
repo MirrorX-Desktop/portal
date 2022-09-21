0057b6bbca0af3b2648152cdbb4452595671bbf1
chenbaiyu <cby40899570@gmail.com>
2022-09-21 18:43:18 +0800

    commit

--- proto/build.rs
+++ proto/build.rs
@@ -1,13 +1,15 @@
 fn main() {
+    // let dir = env!("OUT_DIR");
+
     tonic_build::configure()
-        .out_dir("src")
+        // .out_dir(dir)
         .compile(&["src/service.proto"], &["src"])
         .expect("generate service proto failed");
 
-    let mut message_proto_config = prost_build::Config::default();
-    message_proto_config.out_dir("src");
+    // let mut message_proto_config = prost_build::Config::default();
+    // message_proto_config.out_dir(dir);
 
     prost_reflect_build::Builder::new()
-        .compile_protos_with_config(message_proto_config, &["src/message.proto"], &["."])
+        .compile_protos(&["src/message.proto"], &["."])
         .expect("generate message proto failed");
 }
--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,7 +1,12 @@
 use once_cell::sync::Lazy;
 
-pub mod message;
-pub mod service;
+pub mod message {
+    tonic::include_proto!("message");
+}
+
+pub mod service {
+    tonic::include_proto!("service");
+}
 
 static DESCRIPTOR_POOL: Lazy<prost_reflect::DescriptorPool> = Lazy::new(|| {
     prost_reflect::DescriptorPool::decode(
