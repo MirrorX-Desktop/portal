6c7f904c6dc1b971c4658508ed71b25cf93ba7c9
chenbaiyu <cby40899570@gmail.com>
2022-10-19 01:59:06 +0800

    add visit device id estimate

--- service/src/handlers/visit.rs
+++ service/src/handlers/visit.rs
@@ -4,6 +4,10 @@ use tonic::Status;
 
 #[tracing::instrument]
 pub async fn handle_visit(req: VisitRequest) -> Result<VisitResponse, Status> {
+    if req.active_device_id == req.passive_device_id {
+        return Err(Status::invalid_argument("you can't visit yourself device!"));
+    }
+
     let passive_device_client = CLIENTS
         .get(&req.passive_device_id)
         .ok_or_else(|| Status::not_found("passive device not found"))?;
